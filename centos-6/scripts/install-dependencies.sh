4962474d0a881f6ef4390ba34cc490ea72398d03
Bram Veenboer <bram.veenboer@gmail.com>
2015-07-17 09:43:45 +0200

    Added which as a dependency to let casacore tests pass.

--- centos-6/scripts/install-dependencies.sh
+++ centos-6/scripts/install-dependencies.sh
@@ -14,7 +14,7 @@ yum -y install g++ gcc gcc-c++ gcc-gfortran
 yum -y install blas-devel boost-devel fftw3-devel fftw3-libs python-devel lapack-devel libpng-devel libxml2-devel numpy-devel readline-devel ncurses-devel f2py bzip2-devel libicu-devel scipy
 
 # misc
-yum -y install bison flex ncurses sudo tar bzip2
+yum -y install bison flex ncurses sudo tar bzip2 which
 
 # python packages
 easy_install pyfits
