97a7dddcc8a4eeb34a21c5a951a50c98b48a9c3a
Bram Veenboer <bram.veenboer@gmail.com>
2015-07-17 09:43:47 +0200

    Added initial Ubuntu 12.04 image.

---
+++ ubuntu-12.04/scripts/install-casarest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir ${INSTALLDIR}/casarest
+cd ${INSTALLDIR}/casarest
+svn co --quiet https://svn.astron.nl/casarest/trunk/casarest/ src
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/casarest -DCASACORE_ROOT_DIR=${INSTALLDIR}/casacore -DWCSLIB_ROOT_DIR=${INSTALLDIR}/wcslib -DCFITSIO_ROOT_DIR=${INSTALLDIR}/cfitsio ../src/
+sudo ln -s /usr/lib64 /usr/lib64/lib64
+make -j ${J}
+make install
---
+++ ubuntu-12.04/scripts/install-python-casacore.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir ${INSTALLDIR}/python-casacore
+cd ${INSTALLDIR}/python-casacore
+git clone https://github.com/casacore/python-casacore
+cd python-casacore/
+./setup.py build_ext -I${INSTALLDIR}/wcslib/include:${INSTALLDIR}/casacore/include/:${INSTALLDIR}/cfitsio/include -L${INSTALLDIR}/wcslib/lib:${INSTALLDIR}/casacore/lib/:${INSTALLDIR}/cfitsio/lib/
+mkdir -p ${INSTALLDIR}/python-casacore/lib/python2.7/site-packages/
+mkdir -p ${INSTALLDIR}/python-casacore/lib64/python2.7/site-packages/
+export PYTHONPATH=${INSTALLDIR}/python-casacore/lib/python2.7/site-packages/:${INSTALLDIR}/python-casacore/lib64/python2.7/site-packages/:${PYTHONPATH}
+./setup.py install --prefix=${INSTALLDIR}/python-casacore/
