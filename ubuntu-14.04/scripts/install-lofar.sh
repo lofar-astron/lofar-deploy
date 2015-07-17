12047c5c522faa082091bc52324bb7bebf77d7c2
Yan Grange <grange@astron.nl>
2015-07-17 14:08:28 +0200

    Removed installation of log4cplus installation since it was already done by apt-get.

--- ubuntu-14.04/scripts/install-lofar.sh
+++ ubuntu-14.04/scripts/install-lofar.sh
@@ -4,7 +4,7 @@ cd ${INSTALLDIR}/lofar
 svn --non-interactive -q --username lofar-guest --password lofar-guest co https://svn.astron.nl/LOFAR/trunk src
 mkdir -p build/gnu_opt
 cd build/gnu_opt
-cmake -DBUILD_PACKAGES=Offline -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/lofar/ -DWCSLIB_ROOT_DIR=${INSTALLDIR}/wcslib/ -DCFITSIO_ROOT_DIR=${INSTALLDIR}/cfitsio/ -DCASAREST_ROOT_DIR=${INSTALLDIR}/casarest/ -DCASACORE_ROOT_DIR=${INSTALLDIR}/casacore/ -DLOG4CPLUS_ROOT_DIR=${INSTALLDIR}/log4cplus/ -DUSE_OPENMP=True ${INSTALLDIR}/lofar/src/
+cmake -DBUILD_PACKAGES=Offline -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/lofar/ -DWCSLIB_ROOT_DIR=${INSTALLDIR}/wcslib/ -DCFITSIO_ROOT_DIR=${INSTALLDIR}/cfitsio/ -DCASAREST_ROOT_DIR=${INSTALLDIR}/casarest/ -DCASACORE_ROOT_DIR=${INSTALLDIR}/casacore/ -DUSE_OPENMP=True ${INSTALLDIR}/lofar/src/
 sed -i '29,31d' include/ApplCommon/PosixTime.h
 make -j ${J}
 make install
