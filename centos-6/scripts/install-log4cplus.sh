80892e73de26c4eee1bc63a6e21fbe493daea2a0
Yan Grange <grange@astron.nl>
2015-07-17 16:20:24 +0200

    Added the specific branch (1.1.x) of logc4plus to make the compile succeed

--- centos-6/scripts/install-log4cplus.sh
+++ centos-6/scripts/install-log4cplus.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir ${INSTALLDIR}/log4cplus
 cd ${INSTALLDIR}/log4cplus
-wget http://downloads.sourceforge.net/project/log4cplus/log4cplus-stable/1.1.3/log4cplus-1.1.3-rc4.tar.bz2
-tar xf log4cplus-1.1.3-rc4.tar.bz2
-cd log4cplus-1.1.3-rc4
-./configure --prefix=${INSTALLDIR}/log4cplus
+git clone --recursive git://github.com/log4cplus/log4cplus.git -b 1.1.x src
+mkdir build
+cd build/
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/log4cplus ../src/
 make -j ${J}
 make install
--- centos-7/scripts/install-log4cplus.sh
+++ centos-7/scripts/install-log4cplus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir ${INSTALLDIR}/log4cplus
 cd ${INSTALLDIR}/log4cplus
-git clone --recursive git://github.com/log4cplus/log4cplus.git src
+git clone --recursive git://github.com/log4cplus/log4cplus.git -b 1.1.x src
 mkdir build
 cd build/
 cmake -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/log4cplus ../src/
