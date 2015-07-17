a0d2089903f8b540c92b5b7ff04d676db52d634b
Yan Grange <grange@astron.nl>
2015-07-17 10:05:05 +0200

    Added pip installer for python packages. Added install script to make all casacore and lofar tests pass.

--- ubuntu-12.04/scripts/install-dependencies.sh
+++ ubuntu-12.04/scripts/install-dependencies.sh
@@ -11,14 +11,14 @@ apt-get -y install automake autotools-dev cmake make python-setuptools
 apt-get -y install  g++ gcc gfortran
 
 # libraries
-apt-get -y install libblas-dev libfftw3-dev python-dev liblapack-dev libpng-dev libxml2-dev python-numpy-dev libreadline-dev libncurses-dev python-scipy liblog4cplus-dev 
+apt-get -y install libblas-dev libfftw3-dev python-dev liblapack-dev libpng-dev libxml2-dev python-numpy libreadline-dev libncurses-dev python-scipy liblog4cplus-dev
 
 # minimal boost packages
 apt-get -y install libboost-dev libboost-python-dev libboost-thread-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev
 
 # misc
-apt-get -y install bison bzip2 flex csh
+apt-get -y install bison bzip2 flex csh python-xmlrunner python-pip
 
 # python packages
-easy_install pyfits xmlrunner pywcs python-monetdb
+pip install pyfits pywcs python-monetdb
 
