e0c8bd339119190fe2892e2e658b6ae2e6235f43
Yan Grange <grange@astron.nl>
2015-07-20 21:56:55 +0200

    Renamed make_all_tests_work to make-all-tests-work for uniformity of file names, chmoded to 755 and added some chmod values because of ssh requirements.

--- ubuntu-14.04/scripts/make-all-tests-work.sh
+++ ubuntu-14.04/scripts/make-all-tests-work.sh
@@ -5,3 +5,8 @@ sudo /usr/sbin/sshd
 mkdir -p ${HOME}/.ssh
 ssh-keygen -t rsa -N "" -f ${HOME}/.ssh/id_rsa
 cat ${HOME}/.ssh/id_rsa.pub > ${HOME}/.ssh/authorized_keys
+echo "StrictHostKeyChecking no" > ~/.ssh/config
+chmod 700 ${HOME}/.ssh
+chmod 600 ${HOME}/.ssh/authorized_keys
+chmod 600 ${HOME}/.ssh/id_rsa
+chmod 600 ${HOME}/.ssh/config
