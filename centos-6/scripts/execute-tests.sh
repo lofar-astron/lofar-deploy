2f5b8774ca1084033aab1b6f82ae7374fe8755de
Yan Grange <grange@astron.nl>
2015-07-20 22:37:23 +0200

    Made the exectue-tests.sh so that it will fail if any test fails

--- centos-6/scripts/execute-tests.sh
+++ centos-6/scripts/execute-tests.sh
@@ -1,7 +1,7 @@
-cd ${INSTALLDIR}/casacore/build
-make test
-cd ${INSTALLDIR}/log4cplus/build
-make test
-cd ${INSTALLDIR}/lofar/build/gnu_opt
-make test
-exit 0 # Since we already know some tests will fail in a normal install, we don't want the docker container to fail building. This also allows for only execute the failing tests in a built container with ctest.
+#!/bin/bash
+cd ${INSTALLDIR}/casacore/build &&
+make test &&
+cd ${INSTALLDIR}/log4cplus/build &&
+make test &&
+cd ${INSTALLDIR}/lofar/build/gnu_opt &&
+make test &&
