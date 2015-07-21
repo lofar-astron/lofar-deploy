0fe4f677176f75b5c822ed5f86520f7beb2f2875
Yan Grange <grange@astron.nl>
2015-07-21 11:40:48 +0200

    Non-interactive aptitude now done via environment variable in stead of flag for each apt-get.

--- ubuntu-14.04/scripts/install-dependencies.sh
+++ ubuntu-14.04/scripts/install-dependencies.sh
@@ -1,23 +1,23 @@
 # admin
-apt-get -y install sudo &&
+apt-get install sudo &&
 
 # download
-apt-get -y install git subversion wget &&
+apt-get install git subversion wget &&
 
 # build
-apt-get -y install automake autotools-dev cmake make python-setuptools &&
+apt-get install automake autotools-dev cmake make python-setuptools &&
 
 # compiler
-apt-get -y install  g++ gcc gfortran &&
+apt-get install  g++ gcc gfortran &&
 
 # libraries
-apt-get -y install libblas-dev libfftw3-dev python-dev liblapack-dev libpng-dev libxml2-dev python-numpy libreadline-dev libncurses-dev python-scipy liblog4cplus-dev &&
+apt-get install libblas-dev libfftw3-dev python-dev liblapack-dev libpng-dev libxml2-dev python-numpy libreadline-dev libncurses-dev python-scipy liblog4cplus-dev &&
 
 # minimal boost packages
-apt-get -y install libboost-dev libboost-python-dev libboost-thread-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev &&
+apt-get install libboost-dev libboost-python-dev libboost-thread-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev &&
 
 # misc
-apt-get -y install bison bzip2 flex csh python-xmlrunner python-pip &&
+apt-get install bison bzip2 flex csh python-xmlrunner python-pip &&
 
 # python packages
 pip install pyfits pywcs python-monetdb
