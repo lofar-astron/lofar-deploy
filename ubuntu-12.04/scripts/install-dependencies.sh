4be8360610d22ff83c8114ac95b9caac5307a406
Yan Grange <grange@astron.nl>
2015-07-21 13:14:27 +0200

    -y flag seems to be needed for apt-get even though DEBIAN_FRONTEND=interactive

--- ubuntu-12.04/scripts/install-dependencies.sh
+++ ubuntu-12.04/scripts/install-dependencies.sh
@@ -1,23 +1,23 @@
 # admin
-apt-get install sudo &&
+apt-get -y install sudo &&
 
 # download
-apt-get install git subversion wget &&
+apt-get -y install git subversion wget &&
 
 # build
-apt-get install automake autotools-dev cmake make python-setuptools &&
+apt-get -y install automake autotools-dev cmake make python-setuptools &&
 
 # compiler
-apt-get install  g++ gcc gfortran &&
+apt-get -y install  g++ gcc gfortran &&
 
 # libraries
-apt-get install libblas-dev libfftw3-dev python-dev liblapack-dev libpng-dev libxml2-dev python-numpy libreadline-dev libncurses-dev python-scipy liblog4cplus-dev &&
+apt-get -y install libblas-dev libfftw3-dev python-dev liblapack-dev libpng-dev libxml2-dev python-numpy libreadline-dev libncurses-dev python-scipy liblog4cplus-dev &&
 
 # minimal boost packages
-apt-get install libboost-dev libboost-python-dev libboost-thread-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev &&
+apt-get -y install libboost-dev libboost-python-dev libboost-thread-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev &&
 
 # misc
-apt-get install bison bzip2 flex csh python-xmlrunner python-pip &&
+apt-get -y install bison bzip2 flex csh python-xmlrunner python-pip &&
 
 # python packages
 pip install pyfits pywcs python-monetdb
