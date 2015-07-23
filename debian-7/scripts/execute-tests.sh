a37be8bd7d5dc4814e7e6575546f6f7c09cda467
Bram Veenboer <bram.veenboer@gmail.com>
2015-07-23 14:09:09 +0200

    Removed old directories for debian and ubuntu containers prior to recreating them using the new mechanism.

--- debian-7/scripts/execute-tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ${INSTALLDIR}/casacore/build &&
-make test &&
-cd ${INSTALLDIR}/lofar/build/gnu_opt &&
-make test 
--- debian-7/scripts/setup-account.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-useradd -m ${USER} &&
-usermod -a -G sudo ${USER} &&
-echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
