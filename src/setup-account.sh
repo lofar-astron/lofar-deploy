849307dd3d771572e5718fa6397331fddc8b66e8
Yan Grange <grange@astron.nl>
2015-07-24 11:08:58 +0200

    Added a line to sudoers to allow sudo to be executed without a tty which is needed to use sudo under docker build.

--- src/setup-account.sh
+++ src/setup-account.sh
@@ -2,3 +2,4 @@ getent group sudo &>/dev/null || groupadd sudo
 ${COMMAND_ADD_USER} -m ${USER}
 usermod -a -G sudo ${USER}
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+sed -i 's/requiretty/!requiretty/g' /etc/sudoers
