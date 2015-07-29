b871c874c47c5023aa91d609f41e0cf8331e29f3
Yan Grange <grange@astron.nl>
2015-07-29 15:57:32 +0200

    Removed log4cplus scripts from deblike installs.

--- common/install-log4cplus.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ${INSTALLDIR}/log4cplus/build
-cd ${INSTALLDIR}/log4cplus && git clone https://github.com/log4cplus/log4cplus.git -b ${LOG4CPLUS_VERSION} src
-cd ${INSTALLDIR}/log4cplus/build && cmake -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/log4cplus ../src/
-cd ${INSTALLDIR}/log4cplus/build && make -j ${J}
-cd ${INSTALLDIR}/log4cplus/build && make install
