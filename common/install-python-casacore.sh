aaedb87e91faf4b3c34138909c81d9a954a9d41a
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2015-08-15 20:23:59 +0200

    Install python-casacore properly and run tests

--- common/install-python-casacore.sh
+++ common/install-python-casacore.sh
@@ -1,6 +1,6 @@
 mkdir ${INSTALLDIR}/python-casacore
 cd ${INSTALLDIR}/python-casacore && git clone https://github.com/casacore/python-casacore
-cd ${INSTALLDIR}/python-casacore/python-casacore ./setup.py build_ext -I${INSTALLDIR}/wcslib/include:${INSTALLDIR}/casacore/include/:${INSTALLDIR}/cfitsio/include -L${INSTALLDIR}/wcslib/lib:${INSTALLDIR}/casacore/lib/:${INSTALLDIR}/cfitsio/lib/
+cd ${INSTALLDIR}/python-casacore/python-casacore && ./setup.py build_ext -I${INSTALLDIR}/wcslib/include:${INSTALLDIR}/casacore/include/:${INSTALLDIR}/cfitsio/include -L${INSTALLDIR}/wcslib/lib:${INSTALLDIR}/casacore/lib/:${INSTALLDIR}/cfitsio/lib/
 mkdir -p ${INSTALLDIR}/python-casacore/lib/python${PYTHON_VERSION}/site-packages/
 mkdir -p ${INSTALLDIR}/python-casacore/lib64/python${PYTHON_VERSION}/site-packages/
-cd ${INSTALLDIR}/python-casacore/python-casacore ./setup.py install --prefix=${INSTALLDIR}/python-casacore/
+export PYTHONPATH=${INSTALLDIR}/python-casacore/lib/python${PYTHON_VERSION}/site-packages:${INSTALLDIR}/python-casacore/lib64/python${PYTHON_VERSION}/site-packages:$PYTHONPATH && cd ${INSTALLDIR}/python-casacore/python-casacore && ./setup.py develop --prefix=${INSTALLDIR}/python-casacore/
--- common/run-tests.sh
+++ common/run-tests.sh
@@ -1,2 +1,3 @@
 cd ${INSTALLDIR}/casacore/build && make test 
+export LD_LIBRARY_PATH=${INSTALLDIR}/casacore/lib:$LD_LIBRARY_PATH && export PYTHONPATH=${INSTALLDIR}/python-casacore/lib/python${PYTHON_VERSION}/site-packages:${INSTALLDIR}/python-casacore/lib64/python${PYTHON_VERSION}/site-packages:$PYTHONPATH && cd ${INSTALLDIR}/python-casacore/python-casacore && nosetests
 sudo /usr/sbin/sshd && cd ${INSTALLDIR}/lofar/build/gnu_opt && make test
