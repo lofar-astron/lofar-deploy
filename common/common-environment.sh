99bb000d20303297c0a4cff80ba0d8bfd9f35582
Bram Veenboer <bram.veenboer@gmail.com>
2015-08-24 11:13:49 +0200

    Added version number to download a specific version of lofar.

--- common/common-environment.sh
+++ common/common-environment.sh
@@ -1,4 +1,4 @@
-# User to install software fore
+# User to install software for
 USER lofar
 
 # Directory to install software in
@@ -12,3 +12,4 @@ CFITSIO_VERSION 3370
 WCSLIB_VERSION 5.9
 LOG4CPLUS_VERSION 1.1.x
 CASACORE_VERSION v2.0.3
+LOFAR_VERSION 2_9_1
