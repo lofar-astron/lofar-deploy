9ae0ae8bcd7403adca4ade51fc7b04af2da72772
Bram Veenboer <bram.veenboer@gmail.com>
2015-08-24 13:13:55 +0200

    The J flag is now set automatically to the number of threads on the host system. Furthermore, small refinements to makefile routines.

--- common/routines-common.mk
+++ common/routines-common.mk
@@ -4,3 +4,11 @@ define write-header
 	@echo "#" $(1) >> $(2)
 	@echo "#"      >> $(2)
 endef
+
+# Add number of threads as J flag
+define set-build-options
+	$(call write-header,$(0),$(1))
+	@$(eval J = `cat /proc/cpuinfo | grep processor | wc -l`)
+	@echo "ENV J ${J}" >> ${1}
+	@echo "" >> ${1}
+endef
