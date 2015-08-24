f04b44f6a323afe6067391e07c436a483183d3ce
Bram Veenboer <bram.veenboer@gmail.com>
2015-08-24 15:19:06 +0200

    Casarest is now downloaded from the git repository, since it has a newer version.

--- common/install-casarest.sh
+++ common/install-casarest.sh
@@ -1,5 +1,6 @@
 mkdir -p ${INSTALLDIR}/casarest/build
-cd ${INSTALLDIR}/casarest && svn checkout --quiet https://svn.astron.nl/casarest/release/casarest/release-${CASAREST_VERSION} src
+cd ${INSTALLDIR}/casarest && git clone https://github.com/casacore/casarest.git src
+cd ${INSTALLDIR}/casarest/src && git checkout tags/${CASAREST_VERSION}
 cd ${INSTALLDIR}/casarest/build && cmake -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/casarest -DCASACORE_ROOT_DIR=${INSTALLDIR}/casacore -DWCSLIB_ROOT_DIR=${INSTALLDIR}/wcslib -DCFITSIO_ROOT_DIR=${INSTALLDIR}/cfitsio ../src/
 cd ${INSTALLDIR}/casarest/build && make -j ${J}
 cd ${INSTALLDIR}/casarest/build && make install
