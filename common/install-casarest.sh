1dd248c3661251d869e308173e62350440cbef47
Yan Grange <ygrange@users.noreply.github.com>
2015-09-10 10:09:40 +0200

    Changed casarest install from svn to git
    (to keep master and master-stable in sync)

--- common/install-casarest.sh
+++ common/install-casarest.sh
@@ -1,5 +1,5 @@
 mkdir -p ${INSTALLDIR}/casarest/build
-cd ${INSTALLDIR}/casarest && svn co --quiet https://svn.astron.nl/casarest/trunk/casarest/ src
+cd ${INSTALLDIR}/casarest && git clone https://github.com/casacore/casarest.git src
 cd ${INSTALLDIR}/casarest/build && cmake -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/casarest -DCASACORE_ROOT_DIR=${INSTALLDIR}/casacore -DWCSLIB_ROOT_DIR=${INSTALLDIR}/wcslib -DCFITSIO_ROOT_DIR=${INSTALLDIR}/cfitsio ../src/
 cd ${INSTALLDIR}/casarest/build && make -j ${J}
 cd ${INSTALLDIR}/casarest/build && make install
