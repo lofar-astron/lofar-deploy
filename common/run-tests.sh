49f8151ab08869258e62a9de09ccdc4df9cdff6c
Yan Grange <grange@astron.nl>
2015-09-18 21:02:07 +0200

    Added cfitsio to testing

--- common/run-tests.sh
+++ common/run-tests.sh
@@ -1,3 +1,4 @@
+cd ${INSTALLDIR}/cfitsio/build && make test
 cd ${INSTALLDIR}/casacore/build && make test 
 export LD_LIBRARY_PATH=${INSTALLDIR}/casacore/lib:$LD_LIBRARY_PATH && export PYTHONPATH=${INSTALLDIR}/python-casacore/lib/python${PYTHON_VERSION}/site-packages:${INSTALLDIR}/python-casacore/lib64/python${PYTHON_VERSION}/site-packages:$PYTHONPATH && cd ${INSTALLDIR}/python-casacore/python-casacore && nosetests
 sudo /usr/sbin/sshd && cd ${INSTALLDIR}/lofar/build/gnu_opt && make test
