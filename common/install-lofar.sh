ad94d191471f6d3bcd5853e3a74e5236a177d2b1
Bram Veenboer <bram.veenboer@gmail.com>
2015-10-09 10:13:30 +0200

    Added first version of license using license/add-header-all.sh
    Use license/remove-hader-all.sh to remove again.
    Warning: remove license prior to changing license/header.txt

--- common/install-lofar.sh
+++ common/install-lofar.sh
@@ -1,3 +1,21 @@
+#
+# Copyright (C) 2015
+# This file is part of lofar-profiling.
+# 
+# lofar-profiling is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# lofar-profiling is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with lofar-profiling.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 mkdir -p ${INSTALLDIR}/lofar/build/gnu_opt
 if [ "${LOFAR_VERSION}" = "latest" ]; then cd ${INSTALLDIR}/lofar && svn --non-interactive -q --username lofar-guest --password lofar-guest co https://svn.astron.nl/LOFAR/trunk src; fi
 if [ "${LOFAR_VERSION}" != "latest" ]; then cd ${INSTALLDIR}/lofar && svn --non-interactive -q --username lofar-guest --password lofar-guest co https://svn.astron.nl/LOFAR/tags/LOFAR-Release-${LOFAR_VERSION} src; fi
