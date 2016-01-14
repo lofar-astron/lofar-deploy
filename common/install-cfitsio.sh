135d3dab62c2c87183c0d425b395bbcbb14e2832
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2016-01-14 14:15:00 +0100

    Add anonymous@ for all ftp

--- common/install-cfitsio.sh
+++ common/install-cfitsio.sh
@@ -17,7 +17,7 @@
 #
 
 mkdir -p ${INSTALLDIR}/cfitsio/build
-cd ${INSTALLDIR}/cfitsio && wget --retry-connrefused ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio${CFITSIO_VERSION}.tar.gz
+cd ${INSTALLDIR}/cfitsio && wget --retry-connrefused ftp://anonymous@heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio${CFITSIO_VERSION}.tar.gz
 cd ${INSTALLDIR}/cfitsio && tar xf cfitsio${CFITSIO_VERSION}.tar.gz
 cd ${INSTALLDIR}/cfitsio/build && cmake -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/cfitsio/ ../cfitsio
 cd ${INSTALLDIR}/cfitsio/build && make -j ${J}
--- common/install-wcslib.sh
+++ common/install-wcslib.sh
@@ -17,7 +17,7 @@
 #
 
 mkdir ${INSTALLDIR}/wcslib
-cd ${INSTALLDIR}/wcslib && wget --retry-connrefused ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-${WCSLIB_VERSION}.tar.bz2
+cd ${INSTALLDIR}/wcslib && wget --retry-connrefused ftp://anonymous@ftp.atnf.csiro.au/pub/software/wcslib/wcslib-${WCSLIB_VERSION}.tar.bz2
 cd ${INSTALLDIR}/wcslib && tar xf wcslib-*.tar.bz2
 cd ${INSTALLDIR}/wcslib/wcslib* && ./configure --prefix=${INSTALLDIR}/wcslib --with-cfitsiolib=${INSTALLDIR}/cfitsio/lib/ --with-cfitsioinc=${INSTALLDIR}/cfitsio/include/ --without-pgplot
 cd ${INSTALLDIR}/wcslib/wcslib* && make
