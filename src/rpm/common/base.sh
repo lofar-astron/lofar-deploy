6c5520bc91169eb326d88ab3639edbb650d5494b
Yan Grange <grange@astron.nl>
2016-03-02 14:10:30 +0100

    Removed extra sudo

--- src/rpm/common/base.sh
+++ src/rpm/common/base.sh
@@ -35,7 +35,7 @@ yum -y install g++ gcc gcc-c++ gcc-gfortran
 yum -y install blas-devel boost-devel fftw3-devel fftw3-libs python-devel lapack-devel libpng-devel libxml2-devel numpy-devel readline-devel ncurses-devel f2py bzip2-devel libicu-devel scipy
 
 # misc
-yum -y install bison flex ncurses sudo tar bzip2 which
+yum -y install bison flex ncurses tar bzip2 which
 
 # python packages
 wget --retry-connrefused https://bootstrap.pypa.io/get-pip.py -O - | python
