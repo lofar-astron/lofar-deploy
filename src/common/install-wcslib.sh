f4681450e2769eca4bd9453e423bfb612caa9d23
Bram Veenboer <bram.veenboer@gmail.com>
2019-07-22 14:00:46 +0200

    Fix installation of wcslib, update version from 6.2 to 6.3

--- src/common/install-wcslib.sh
+++ src/common/install-wcslib.sh
@@ -17,8 +17,8 @@
 #
 
 mkdir ${INSTALLDIR}/wcslib
-cd ${INSTALLDIR}/wcslib && wget --retry-connrefused ftp://anonymous@ftp.atnf.csiro.au/pub/software/wcslib/wcslib.tar.bz2 -O wcslib-${WCSLIB_VERSION}.tar.bz2
-cd ${INSTALLDIR}/wcslib && tar xf wcslib-*.tar.bz2
-cd ${INSTALLDIR}/wcslib/wcslib* && ./configure --prefix=${INSTALLDIR}/wcslib --with-cfitsiolib=${INSTALLDIR}/cfitsio/lib/ --with-cfitsioinc=${INSTALLDIR}/cfitsio/include/ --without-pgplot
+cd ${INSTALLDIR}/wcslib && wget --retry-connrefused ftp://anonymous@ftp.atnf.csiro.au/pub/software/wcslib/wcslib-${WCSLIB_VERSION}.tar.bz2 -O wcslib-${WCSLIB_VERSION}.tar.bz2
+cd ${INSTALLDIR}/wcslib && tar xf wcslib-${WCSLIB_VERSION}.tar.bz2
+cd ${INSTALLDIR}/wcslib/wcslib-${WCSLIB_VERSION} && ./configure --prefix=${INSTALLDIR}/wcslib --with-cfitsiolib=${INSTALLDIR}/cfitsio/lib/ --with-cfitsioinc=${INSTALLDIR}/cfitsio/include/ --without-pgplot
 cd ${INSTALLDIR}/wcslib/wcslib* && make
 cd ${INSTALLDIR}/wcslib/wcslib* && make install
