e4effad2c453dffc857d1488a5899570e19c267e
Bram Veenboer <bram.veenboer@gmail.com>
2019-07-22 13:52:50 +0200

    Add installation of deltarpm to rpm base.sh

--- src/rpm/common/base.sh
+++ src/rpm/common/base.sh
@@ -1,17 +1,17 @@
 #
 # Copyright (C) 2015
 # This file is part of lofar-profiling.
-# 
+#
 # lofar-profiling is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # lofar-profiling is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with lofar-profiling.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -19,11 +19,14 @@
 # base
 yum -y update
 
+# deltarpm
+yum -y install deltarpm
+
 # admin
 yum -y install sudo
 
 # download
-yum -y install git svn wget 
+yum -y install git svn wget
 
 # build
 yum -y install automake-devel aclocal autoconf autotools make
