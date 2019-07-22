a1661f14a9b72e2836fa7e41b40ddcff02218c75
Bram Veenboer <bram.veenboer@gmail.com>
2019-07-22 16:44:13 +0200

    Add -DBOOST_ROOT to casacore cmake commandline

--- src/common/install-casacore.sh
+++ src/common/install-casacore.sh
@@ -1,17 +1,17 @@
 #
 # Copyright (C) 2015
 # This file is part of lofar-profiling.
-# 
+#
 # lofar-profiling is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # lofar-profiling is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with lofar-profiling.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -22,6 +22,6 @@ cd ${INSTALLDIR}/casacore && git clone https://github.com/casacore/casacore.git 
 if [ "${CASACORE_VERSION}" != "latest" ]; then cd ${INSTALLDIR}/casacore/src && git checkout tags/${CASACORE_VERSION}; fi
 cd ${INSTALLDIR}/casacore/data && wget --retry-connrefused ftp://anonymous@ftp.astron.nl/outgoing/Measures/WSRT_Measures.ztar
 cd ${INSTALLDIR}/casacore/data && tar xf WSRT_Measures.ztar
-cd ${INSTALLDIR}/casacore/build && cmake -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/casacore/ -DDATA_DIR=${INSTALLDIR}/casacore/data -DWCSLIB_ROOT_DIR=/${INSTALLDIR}/wcslib/ -DCFITSIO_ROOT_DIR=${INSTALLDIR}/cfitsio/ -DBUILD_PYTHON=True -DUSE_OPENMP=True -DUSE_FFTW3=TRUE ../src/
+cd ${INSTALLDIR}/casacore/build && cmake -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/casacore/ -DDATA_DIR=${INSTALLDIR}/casacore/data -DWCSLIB_ROOT_DIR=/${INSTALLDIR}/wcslib/ -DCFITSIO_ROOT_DIR=${INSTALLDIR}/cfitsio/ -DBUILD_PYTHON=True -DUSE_OPENMP=True -DUSE_FFTW3=TRUE -DBOOST_ROOT=${INSTALLDIR}/boost/ ../src/
 cd ${INSTALLDIR}/casacore/build && make -j ${J}
 cd ${INSTALLDIR}/casacore/build && make install
